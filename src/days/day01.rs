89cdc189cc8e41f686d658c5d7dd0703a2f7b43f
Matt Friede <7852262+Friede80@users.noreply.github.com>
2024-12-03 06:13:19 -0500

    Day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,12 +1,37 @@
+use itertools::Itertools;
+
 use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let file = File::open("input/day01.txt").unwrap();
+    let reader = BufReader::new(file);
+    let (mut c1, mut c2): (Vec<_>, Vec<_>) = reader
+        .lines()
+        .map(|line| parse_tuple(&line.unwrap()))
+        .unzip();
+
+    c1.sort();
+    c2.sort();
+
+    let sol1: u64 = c1.iter().zip(c2.iter()).map(|(x, y)| x.abs_diff(*y)).sum();
+
+    let frequency: HashMap<u64, usize> =
+        c2.iter().dedup_with_count().map(|(x, y)| (*y, x)).collect();
+
+    let sol2: u64 = c1
+        .into_iter()
+        .map(|x| x * (frequency.get(&x).map_or(0, |y| *y) as u64))
+        .sum();
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn parse_tuple(line: &str) -> (u64, u64) {
+    let mut iter = line.split_whitespace().map(|x| x.parse::<u64>());
+    (iter.next().unwrap().unwrap(), iter.next().unwrap().unwrap())
 }
