4ac97970bc7bc306fd7892d0d6eb9982c31afa59
Matt Friede <7852262+Friede80@users.noreply.github.com>
2024-12-03 12:01:21 -0500

    Day 3

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -5,33 +5,36 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+use std::fs::read_to_string;
+
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    let file = File::open("input/day01.txt").unwrap();
-    let reader = BufReader::new(file);
-    let (mut c1, mut c2): (Vec<_>, Vec<_>) = reader
+    // let file = File::open("input/day01.txt").unwrap();
+    // let reader = BufReader::new(file);
+    let input = read_to_string("input/day01.txt").unwrap();
+    let (mut c1, mut c2): (Vec<_>, Vec<_>) = input
         .lines()
-        .map(|line| parse_tuple(&line.unwrap()))
+        .map(|line| {
+            line.split_whitespace()
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
         .unzip();
 
     c1.sort();
     c2.sort();
 
-    let sol1: u64 = c1.iter().zip(c2.iter()).map(|(x, y)| x.abs_diff(*y)).sum();
+    let sol1: usize = c1.iter().zip(c2.iter()).map(|(x, y)| x.abs_diff(*y)).sum();
 
-    let frequency: HashMap<u64, usize> =
+    let frequency: HashMap<usize, usize> =
         c2.iter().dedup_with_count().map(|(x, y)| (*y, x)).collect();
 
-    let sol2: u64 = c1
+    let sol2: usize = c1
         .into_iter()
-        .map(|x| x * (frequency.get(&x).map_or(0, |y| *y) as u64))
+        .map(|x| x * frequency.get(&x).map_or(0, |y| *y))
         .sum();
 
     (Solution::from(sol1), Solution::from(sol2))
-}
-
-fn parse_tuple(line: &str) -> (u64, u64) {
-    let mut iter = line.split_whitespace().map(|x| x.parse::<u64>());
-    (iter.next().unwrap().unwrap(), iter.next().unwrap().unwrap())
 }
--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,12 +1,47 @@
 use crate::{Solution, SolutionPair};
+
+use regex::Regex;
 use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = read_to_string("input/day03.txt").unwrap();
+
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let sol1: u64 = re
+        .captures_iter(&input)
+        .map(|cap| {
+            let x = cap[1].parse::<u64>().unwrap();
+            let y = cap[2].parse::<u64>().unwrap();
+            x * y
+        })
+        .sum();
+
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)").unwrap();
+    let mut mult_enabled = true;
+    let sol2: u64 = re
+        .captures_iter(&input)
+        .map(|cap| match &cap[0] {
+            "do()" => {
+                mult_enabled = true;
+                0
+            }
+            "don't()" => {
+                mult_enabled = false;
+                0
+            }
+            _ => {
+                if mult_enabled {
+                    let x = cap[1].parse::<u64>().unwrap();
+                    let y = cap[2].parse::<u64>().unwrap();
+                    x * y
+                } else {
+                    0
+                }
+            }
+        })
+        .sum();
 
     (Solution::from(sol1), Solution::from(sol2))
 }
